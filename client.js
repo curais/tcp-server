3fde9a76b32b35042b5907df05053283ad910a7b
Cris <craisochoa22@gmail.com>
2019-09-30 08:17:59 -0600

    Implemented a simulted chat

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const net = require('net');
-
+const readline = require('readline');
 const client = new net.Socket();
 
 client.connect(3000, '127.0.0.1', ()=>{
@@ -9,7 +9,6 @@ client.connect(3000, '127.0.0.1', ()=>{
 client.on('data', (data)=>{
     //console.log('Info: ' + data); //Concatenacion
     console.log(`Server: ${data}`); //Interpolacion
-    client.destroy();
 });
 
 client.on('close', ()=>{
@@ -17,4 +16,14 @@ client.on('close', ()=>{
 });
 
 
+//hacer chat
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+rl.on('line', (line) => {
+    client.write(line);
+});
+
+rl.prompt();
